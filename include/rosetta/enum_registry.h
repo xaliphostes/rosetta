e6c48876a6bc0dbbdde38c4a9dcf57982a85d9d4
Xaliphostes <fmaerten@gmail.com>
2025-10-06 19:25:40 +0200

    before enums

--- include/rosetta/enum_registry.h
+++ include/rosetta/enum_registry.h
@@ -4,10 +4,10 @@
  */
 #pragma once
 #include <string>
+#include <type_traits>
+#include <typeindex>
 #include <unordered_map>
 #include <vector>
-#include <typeindex>
-#include <type_traits>
 
 namespace rosetta {
 
@@ -16,10 +16,9 @@ namespace rosetta {
      */
     struct EnumValueInfo {
         std::string name;
-        int64_t value;
-        
-        EnumValueInfo(const std::string& n, int64_t v) 
-            : name(n), value(v) {}
+        int64_t     value;
+
+        EnumValueInfo(const std::string &n, int64_t v) : name(n), value(v) {}
     };
 
     /**
@@ -27,21 +26,20 @@ namespace rosetta {
      */
     class EnumInfo {
     public:
-        std::string name;
-        std::vector<EnumValueInfo> values;
+        std::string                              name;
+        std::vector<EnumValueInfo>               values;
         std::unordered_map<std::string, int64_t> name_to_value;
         std::unordered_map<int64_t, std::string> value_to_name;
 
-        explicit EnumInfo(const std::string& enum_name) 
-            : name(enum_name) {}
+        explicit EnumInfo(const std::string &enum_name) : name(enum_name) {}
 
-        void addValue(const std::string& value_name, int64_t value) {
+        void addValue(const std::string &value_name, int64_t value) {
             values.emplace_back(value_name, value);
             name_to_value[value_name] = value;
-            value_to_name[value] = value_name;
+            value_to_name[value]      = value_name;
         }
 
-        bool hasValue(const std::string& value_name) const {
+        bool hasValue(const std::string &value_name) const {
             return name_to_value.find(value_name) != name_to_value.end();
         }
 
@@ -49,12 +47,13 @@ namespace rosetta {
             return value_to_name.find(value) != value_to_name.end();
         }
 
-        int64_t getValue(const std::string& value_name) const {
+        int64_t getValue(const std::string &value_name) const {
             auto it = name_to_value.find(value_name);
             if (it != name_to_value.end()) {
                 return it->second;
             }
-            throw std::runtime_error("Enum value '" + value_name + "' not found in enum '" + name + "'");
+            throw std::runtime_error("Enum value '" + value_name + "' not found in enum '" + name +
+                                     "'");
         }
 
         std::string getName(int64_t value) const {
@@ -62,7 +61,8 @@ namespace rosetta {
             if (it != value_to_name.end()) {
                 return it->second;
             }
-            throw std::runtime_error("Enum value " + std::to_string(value) + " not found in enum '" + name + "'");
+            throw std::runtime_error("Enum value " + std::to_string(value) +
+                                     " not found in enum '" + name + "'");
         }
     };
 
@@ -71,42 +71,38 @@ namespace rosetta {
      */
     class EnumRegistry {
     public:
-        static EnumRegistry& instance();
+        static EnumRegistry &instance();
+
+        template <typename EnumType> void registerEnum(const std::string &enum_name);
 
         template <typename EnumType>
-        void registerEnum(const std::string& enum_name);
+        void addEnumValue(const std::string &value_name, EnumType value);
 
-        template <typename EnumType>
-        void addEnumValue(const std::string& value_name, EnumType value);
+        template <typename EnumType> const EnumInfo *getEnumInfo() const;
 
-        template <typename EnumType>
-        const EnumInfo* getEnumInfo() const;
+        const EnumInfo *getEnumInfo(const std::string &enum_name) const;
 
-        const EnumInfo* getEnumInfo(const std::string& enum_name) const;
-
-        template <typename EnumType>
-        bool isRegistered() const;
+        template <typename EnumType> bool isRegistered() const;
 
         std::vector<std::string> getAllEnumNames() const;
 
     private:
         EnumRegistry() = default;
-        std::unordered_map<std::type_index, EnumInfo> enums_by_type;
+        std::unordered_map<std::type_index, EnumInfo>    enums_by_type;
         std::unordered_map<std::string, std::type_index> enums_by_name;
     };
 
     /**
      * @brief Helper class for fluent enum registration
      */
-    template <typename EnumType>
-    class EnumRegistrar {
+    template <typename EnumType> class EnumRegistrar {
     public:
-        explicit EnumRegistrar(const std::string& enum_name) {
+        explicit EnumRegistrar(const std::string &enum_name) {
             EnumRegistry::instance().registerEnum<EnumType>(enum_name);
             enum_name_ = enum_name;
         }
 
-        EnumRegistrar& value(const std::string& name, EnumType val) {
+        EnumRegistrar &value(const std::string &name, EnumType val) {
             EnumRegistry::instance().addEnumValue(name, val);
             return *this;
         }
@@ -124,7 +120,7 @@ namespace rosetta {
 /**
  * @brief Primary macro for registering enum class with values
  * This is the RECOMMENDED method - most reliable
- * 
+ *
  * Usage:
  * enum class Status { Active, Inactive, Pending };
  * BEGIN_ENUM_REGISTRATION(Status)
@@ -133,24 +129,24 @@ namespace rosetta {
  *     ENUM_VALUE(Pending)
  * END_ENUM_REGISTRATION()
  */
-#define BEGIN_ENUM_REGISTRATION(EnumType)                                     \
-    namespace {                                                               \
-        struct EnumType##_Registrar {                                         \
-            using EnumType_t = EnumType;                                      \
-            rosetta::EnumRegistrar<EnumType> reg;                             \
+#define BEGIN_ENUM_REGISTRATION(EnumType)         \
+    namespace {                                   \
+        struct EnumType##_Registrar {             \
+            using EnumType_t = EnumType;          \
+            rosetta::EnumRegistrar<EnumType> reg; \
             EnumType##_Registrar() : reg(#EnumType) {
 
-#define ENUM_VALUE(ValueName)                                                 \
-                reg.value(#ValueName, EnumType_t::ValueName);
+#define ENUM_VALUE(ValueName) reg.value(#ValueName, EnumType_t::ValueName);
 
-#define END_ENUM_REGISTRATION()                                               \
-            }                                                                 \
-        } enum_registrar_instance;                                            \
+#define END_ENUM_REGISTRATION() \
+    }                           \
+    }                           \
+    enum_registrar_instance;    \
     }
 
 /**
  * @brief Alternative macro for plain enums (not enum class)
- * 
+ *
  * Usage:
  * enum Color { Red, Green, Blue };
  * BEGIN_PLAIN_ENUM_REGISTRATION(Color)
@@ -159,112 +155,111 @@ namespace rosetta {
  *     PLAIN_ENUM_VALUE(Blue)
  * END_PLAIN_ENUM_REGISTRATION()
  */
-#define BEGIN_PLAIN_ENUM_REGISTRATION(EnumType)                               \
-    namespace {                                                               \
-        struct EnumType##_Registrar {                                         \
-            rosetta::EnumRegistrar<EnumType> reg;                             \
+#define BEGIN_PLAIN_ENUM_REGISTRATION(EnumType)   \
+    namespace {                                   \
+        struct EnumType##_Registrar {             \
+            rosetta::EnumRegistrar<EnumType> reg; \
             EnumType##_Registrar() : reg(#EnumType) {
 
-#define PLAIN_ENUM_VALUE(ValueName)                                           \
-                reg.value(#ValueName, ValueName);
+#define PLAIN_ENUM_VALUE(ValueName) reg.value(#ValueName, ValueName);
 
-#define END_PLAIN_ENUM_REGISTRATION()                                         \
-            }                                                                 \
-        } enum_registrar_instance;                                            \
+#define END_PLAIN_ENUM_REGISTRATION() \
+    }                                 \
+    }                                 \
+    enum_registrar_instance;          \
     }
 
 /**
  * @brief Helper macro for single value registration (internal use)
  */
-#define ROSETTA_ENUM_VALUE_IMPL(EnumType, ValueName) \
-    reg.value(#ValueName, EnumType::ValueName);
+#define ROSETTA_ENUM_VALUE_IMPL(EnumType, ValueName) reg.value(#ValueName, EnumType::ValueName);
 
 /**
  * @brief Convenient one-liner registration for 1-6 enum values
- * 
+ *
  * Usage:
  * REGISTER_ENUM_3(Status, Active, Inactive, Pending)
  * REGISTER_ENUM_4(Priority, Low, Medium, High, Critical)
  */
-#define REGISTER_ENUM_1(EnumType, a)                                          \
-    namespace {                                                               \
-        struct EnumType##_Registrar {                                         \
-            EnumType##_Registrar() {                                          \
-                rosetta::EnumRegistrar<EnumType> reg(#EnumType);              \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, a)                          \
-            }                                                                 \
-        };                                                                    \
-        static EnumType##_Registrar EnumType##_registrar_instance;            \
+#define REGISTER_ENUM_1(EnumType, a)                               \
+    namespace {                                                    \
+        struct EnumType##_Registrar {                              \
+            EnumType##_Registrar() {                               \
+                rosetta::EnumRegistrar<EnumType> reg(#EnumType);   \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, a)               \
+            }                                                      \
+        };                                                         \
+        static EnumType##_Registrar EnumType##_registrar_instance; \
     }
 
-#define REGISTER_ENUM_2(EnumType, a, b)                                       \
-    namespace {                                                               \
-        struct EnumType##_Registrar {                                         \
-            EnumType##_Registrar() {                                          \
-                rosetta::EnumRegistrar<EnumType> reg(#EnumType);              \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, a)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, b)                          \
-            }                                                                 \
-        };                                                                    \
-        static EnumType##_Registrar EnumType##_registrar_instance;            \
+#define REGISTER_ENUM_2(EnumType, a, b)                            \
+    namespace {                                                    \
+        struct EnumType##_Registrar {                              \
+            EnumType##_Registrar() {                               \
+                rosetta::EnumRegistrar<EnumType> reg(#EnumType);   \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, a)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, b)               \
+            }                                                      \
+        };                                                         \
+        static EnumType##_Registrar EnumType##_registrar_instance; \
     }
 
-#define REGISTER_ENUM_3(EnumType, a, b, c)                                    \
-    namespace {                                                               \
-        struct EnumType##_Registrar {                                         \
-            EnumType##_Registrar() {                                          \
-                rosetta::EnumRegistrar<EnumType> reg(#EnumType);              \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, a)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, b)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, c)                          \
-            }                                                                 \
-        };                                                                    \
-        static EnumType##_Registrar EnumType##_registrar_instance;            \
+#define REGISTER_ENUM_3(EnumType, a, b, c)                         \
+    namespace {                                                    \
+        struct EnumType##_Registrar {                              \
+            EnumType##_Registrar() {                               \
+                rosetta::EnumRegistrar<EnumType> reg(#EnumType);   \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, a)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, b)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, c)               \
+            }                                                      \
+        };                                                         \
+        static EnumType##_Registrar EnumType##_registrar_instance; \
     }
 
-#define REGISTER_ENUM_4(EnumType, a, b, c, d)                                 \
-    namespace {                                                               \
-        struct EnumType##_Registrar {                                         \
-            EnumType##_Registrar() {                                          \
-                rosetta::EnumRegistrar<EnumType> reg(#EnumType);              \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, a)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, b)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, c)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, d)                          \
-            }                                                                 \
-        };                                                                    \
-        static EnumType##_Registrar EnumType##_registrar_instance;            \
+#define REGISTER_ENUM_4(EnumType, a, b, c, d)                      \
+    namespace {                                                    \
+        struct EnumType##_Registrar {                              \
+            EnumType##_Registrar() {                               \
+                rosetta::EnumRegistrar<EnumType> reg(#EnumType);   \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, a)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, b)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, c)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, d)               \
+            }                                                      \
+        };                                                         \
+        static EnumType##_Registrar EnumType##_registrar_instance; \
     }
 
-#define REGISTER_ENUM_5(EnumType, a, b, c, d, e)                              \
-    namespace {                                                               \
-        struct EnumType##_Registrar {                                         \
-            EnumType##_Registrar() {                                          \
-                rosetta::EnumRegistrar<EnumType> reg(#EnumType);              \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, a)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, b)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, c)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, d)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, e)                          \
-            }                                                                 \
-        };                                                                    \
-        static EnumType##_Registrar EnumType##_registrar_instance;            \
+#define REGISTER_ENUM_5(EnumType, a, b, c, d, e)                   \
+    namespace {                                                    \
+        struct EnumType##_Registrar {                              \
+            EnumType##_Registrar() {                               \
+                rosetta::EnumRegistrar<EnumType> reg(#EnumType);   \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, a)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, b)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, c)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, d)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, e)               \
+            }                                                      \
+        };                                                         \
+        static EnumType##_Registrar EnumType##_registrar_instance; \
     }
 
-#define REGISTER_ENUM_6(EnumType, a, b, c, d, e, f)                           \
-    namespace {                                                               \
-        struct EnumType##_Registrar {                                         \
-            EnumType##_Registrar() {                                          \
-                rosetta::EnumRegistrar<EnumType> reg(#EnumType);              \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, a)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, b)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, c)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, d)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, e)                          \
-                ROSETTA_ENUM_VALUE_IMPL(EnumType, f)                          \
-            }                                                                 \
-        };                                                                    \
-        static EnumType##_Registrar EnumType##_registrar_instance;            \
+#define REGISTER_ENUM_6(EnumType, a, b, c, d, e, f)                \
+    namespace {                                                    \
+        struct EnumType##_Registrar {                              \
+            EnumType##_Registrar() {                               \
+                rosetta::EnumRegistrar<EnumType> reg(#EnumType);   \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, a)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, b)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, c)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, d)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, e)               \
+                ROSETTA_ENUM_VALUE_IMPL(EnumType, f)               \
+            }                                                      \
+        };                                                         \
+        static EnumType##_Registrar EnumType##_registrar_instance; \
     }
 
 /**
